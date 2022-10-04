e093f48e16c3ae7cc599cfda33d59ebd27808a4d
Moritz Schepp <moritz.schepp@gmail.com>
2022-10-03 17:55:23 -0700

    adds map column for demo

--- app.js
+++ app.js
@@ -13,6 +13,57 @@ function linkify(content) {
   }
 }
 
+function mappify(selector) {
+  let map = null
+  let dl = null
+
+  return function() {
+    let geoJSON = {
+      type: "FeatureCollection",
+      features: []
+    }
+
+    for (const element of document.querySelectorAll(selector)) {
+      const lat = element.querySelector('geo').textContent.split(',')[1]
+      const lon = element.querySelector('geo').textContent.split(',')[0]
+
+      if (!lat || !lon) {continue}
+
+      geoJSON['features'].push({
+        type: 'Feature',
+        properties: {
+          name: element.querySelector('settlement').textContent
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [lat, lon]
+        }
+      })
+    }
+
+    if (!map) {
+      const target = document.querySelector('#map')
+      map = L.map(target).setView([50, 10], 4)
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map)
+    }
+
+    if (dl) {
+      dl.remove()
+    }
+
+    if (geoJSON['features'].length > 0) {
+      dl = L.geoJSON(geoJSON).addTo(map)
+      map.flyToBounds(dl.getBounds(), {maxZoom: 7})
+    } else {
+      map.setView([50, 10], 4)
+    }
+  }
+}
+
 new TeiViewer('[is=viewer]', {
   src: 'data/content.xml',
   processors: [
@@ -23,5 +74,8 @@ new TeiViewer('[is=viewer]', {
     wrapAll("rs[type='artwork']", 'palette-fill', 'artwork'),
     linkify,
     indexW
+  ],
+  postProcessors: [
+    mappify("placeName")
   ]
 })
